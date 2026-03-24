966cfcab570c3abe20412a141c257ce1ecf942df
yjot <2010guabo@gmail.com>
2026-03-24 15:08:13 -0500

    Mejoras del código

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yjotdev.login"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
         testInstrumentationRunner = "com.yjotdev.login.CustomTestRunner"
     }
     signingConfigs {
