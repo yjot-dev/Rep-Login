8c533298c4d8f7a7c4f3977cf5a4b898b98bd7e4
yjot2 <2010guabo@gmail.com>
2026-06-10 15:30:09 -0500

    Subida del codigo de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yjotdev.login"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
         testInstrumentationRunner = "com.yjotdev.login.CustomTestRunner"
     }
     signingConfigs {
